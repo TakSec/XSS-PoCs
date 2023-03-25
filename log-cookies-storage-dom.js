4e2898e8ecaffed6e1bf15c73ed9df0721e845a4
TakSec <michaeljtakahashi@gmail.com>
2023-03-25 16:58:55 -0700

    Update log-cookies-storage-dom.js

--- log-cookies-storage-dom.js
+++ log-cookies-storage-dom.js
@@ -1,3 +1,4 @@
+// Log cookies, local storage, and DOM contents
 console.log(`%cHacked Cookies: %c${document.cookie}`, 'color: red', 'color: blue');
 console.log(`%cHacked Local Storage: %c${JSON.stringify(localStorage)}`, 'color: red', 'color: blue');
 console.log(`%cHacked DOM Contents: %c${document.documentElement.innerHTML}`, 'color: red', 'color: blue');
